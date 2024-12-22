5eb8fb3eb3d204f3bd1e5eb7e9084f41ed4c9e92
Wes Galbraith <wes@MacBookPro.fios-router.home>
2024-12-22 00:48:50 -0500

    Add background image and icons to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,8 @@
+import mountainStars from '../../public/mountain-stars.jpg'
+import sunIcon from '../../public/sun.svg'
+import garlicIcon from '../../public/garlic.svg'
+import Image from 'next/image'
+
 interface HeroProps {
     title: string,
     // img: string,
@@ -7,9 +12,19 @@ export default function Hero(
     {title}: HeroProps
 ) {
     return (
-        <section className="flex flex-col justify-center items-center w-full h-full">
-            <h1 className="text-9xl text-yellow">{title}</h1>
-        </section>
+        <>
+            <section className="flex flex-col justify-center items-center w-full h-full">
+                <h1 className="text-9xl text-yellow">{title}</h1>
+                <div className="flex flex-row gap-6">
+                <Image src={sunIcon} alt='A sun icon' height={120} width={120}/>
+                <Image src={garlicIcon} alt='A garlic icon' height={120} width={120} />
+                </div>
+            </section>
+            {/* opaque overlay */}
+            <div className="-z-40 min-h-full min-w-96 w-full h-auto fixed top-0 left-0 bg-purple-dark opacity-40"/>
+            {/* full screen background image */}
+            <Image className="-z-50 min-h-full min-w-96 w-full h-auto fixed top-0 left-0" src={mountainStars} alt='A starry night in the mountains.' />
+        </>
     )
 
 }
