d8eb71a4d870ffaa3c2328c1a8faef687b9eb3ca
Wes Galbraith <wes@MacBookPro.fios-router.home>
2024-12-24 15:43:15 -0500

    Add nav drawer functionality on mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import sunIcon from '../../public/sun.svg'
 import garlicIcon from '../../public/garlic.svg'
 import hamburgerIcon from '../../public/hamburger.svg'
+import closeIcon from '../../public/close.svg'
 import Link from "next/link"
 import Image from "next/image"
 import {
@@ -26,6 +27,11 @@ interface NavProps {
 // TODO: change purple and yellow to more generic names for colors according to a theme
 
 export default function Nav({title, items}: NavProps) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const openMenu = () => setIsMenuOpen(true);
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
         <>
         {/* nav for large breakpoint and up */}
@@ -35,18 +41,32 @@ export default function Nav({title, items}: NavProps) {
                 <h2>{title}</h2>
                 <Image src={garlicIcon} alt="Garlic icon" height={25} width={25} />
             </Link>    
-            <NavLinks items={items} linkSize='text-2xl'/>
+            <NavLinks items={items} linkSize='text-2xl' onClick={closeMenu}/>
             <SocialLinks/>
         </nav>
         {/* nav for mobile */}
-        <nav className="lg:hidden flex justify-between items-center h-20 p-8 bg-purple-dark border-b-2 border-b-purple-light">
-            <Link href="/" className="text-2xl text-yellow flex flex-row gap-2">
-                <Image src={sunIcon} alt="Sun icon" height={25} width={25}/>
-                <h2>{title}</h2>
-                <Image src={garlicIcon} alt="Garlic icon" height={25} width={25} />
-            </Link>    
-            <Image src={hamburgerIcon} height={30} width={30} alt="Hamburger icon"/>
-        </nav>
+        <div className="lg:hidden flex flex-col">
+            <div className="flex flex-row justify-between items-center h-20 p-8 bg-purple-dark border-b-2 border-b-purple-light">
+                <Link href="/" className="text-2xl text-yellow flex flex-row gap-2">
+                    <Image src={sunIcon} alt="Sun icon" height={25} width={25}/>
+                    <h2>{title}</h2>
+                    <Image src={garlicIcon} alt="Garlic icon" height={25} width={25} />
+                </Link>
+                {
+                    isMenuOpen ? (
+                        <Image onClick={closeMenu} src={closeIcon} height={40} width={40} alt="Exit icon"/>
+                    ) : (
+                        <Image onClick={openMenu} src={hamburgerIcon} height={30} width={30} alt="Hamburger icon"/>
+                    )
+                }
+            </div>
+            {isMenuOpen && (
+                <nav className="bg-purple-dark border-b-2 border-b-purple-light p-4">
+                        {/* // menu drawer with nav links */}
+                        <NavLinks mobile items={items} linkSize="text-4xl" onClick={closeMenu}/>
+                </nav>
+            )}
+        </div>
         </>
     )
 }
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from "next/link"
 
 import {NavItem} from '@/types'
@@ -5,20 +7,30 @@ import {NavItem} from '@/types'
 interface NavLinksProps {
     items: Array<NavItem>
     linkSize?: string
+    mobile?: boolean
+    onClick?: () => void
 }
 
 
-export default function NavLinks({items, linkSize = "text-xl"}: NavLinksProps) {
+export default function NavLinks({items, onClick, linkSize = "text-xl", mobile = false}: NavLinksProps) {
     const listItems = items.map(item => (
         <li key={item.title} className={`${linkSize} text-yellow hover:text-yellow-light hover:underline`}>
-            <Link href={item.href}>{item.title}</Link>
+            <Link href={item.href} onClick={() => onClick && onClick()}>{item.title}</Link>
         </li>
     ))
+
+    if (mobile) {
+        return (
+            <ul className="flex flex-col items-center gap-4">
+                {listItems}
+            </ul>
+
+        )
+    }
 
     return (
         <ul className="flex flex-row gap-4 lg:gap-8">
             {listItems}
         </ul>
-
     )        
 }
