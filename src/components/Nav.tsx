a3acc71c65702b8210534b085dbe26ba57f186d2
Wes Galbraith <wes@Wess-MacBook-Pro.local>
2025-01-11 10:49:39 -0700

    Refactor nav to not hide navs for different screen sizes and fix logo position on mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,6 +11,7 @@ import { NavItem } from "@/types";
 import NavLinks from "@/components/NavLinks";
 import SocialLinks from "@/components/SocialLinks";
 import { teko } from "@/fonts";
+import useBreakpoint from "@/hooks/useBreakpoint";
 
 interface NavProps {
   items: Array<NavItem>;
@@ -19,68 +20,78 @@ interface NavProps {
 
 // TODO: change purple and yellow to more generic names for colors according to a theme
 
-export default function Nav({ items }: NavProps) {
+export default function Nav({ items, breakpoint }: NavProps) {
+  const { isBreakpointOrAbove } = useBreakpoint();
+
+  // TODO: refactor to use breakpoint hooks instead of rendering multiple navs
+
+  if (isBreakpointOrAbove("xl")) {
+    return <NavDesktop items={items} breakpoint={breakpoint} />;
+  }
+  return <NavMobile items={items} breakpoint={breakpoint} />;
+}
+
+function NavDesktop({ items }: NavProps) {
+  return (
+    <nav
+      className={`xl:flex justify-between items-center h-24 pr-8 bg-purple-dark border-b-2 border-b-purple-light ${teko.className}`}
+    >
+      <Link
+        href="/"
+        className="text-2xl text-yellow flex flex-row gap-2 h-full"
+      >
+        <Image src={logo} alt="Solar Garlic Logo" height={50} width={100} />
+      </Link>
+      <NavLinks items={items} linkSize="text-4xl" />
+      <SocialLinks />
+    </nav>
+  );
+}
+
+function NavMobile({ items }: NavProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const openMenu = () => setIsMenuOpen(true);
   const closeMenu = () => setIsMenuOpen(false);
 
-  // TODO: refactor to use breakpoint hooks instead of rendering multiple navs
-
   return (
-    <>
-      {/* nav for xl breakpoint and up */}
-      <nav
-        className={`hidden xl:flex justify-between items-center h-24 pr-8 bg-purple-dark border-b-2 border-b-purple-light ${teko.className}`}
-      >
+    <nav className={`flex flex-col ${teko.className}`}>
+      <div className="pl-0 pr-3 flex flex-row justify-between items-center h-24 bg-purple-dark border-b-2 border-b-purple-light">
         <Link
           href="/"
           className="text-2xl text-yellow flex flex-row gap-2 h-full"
         >
           <Image src={logo} alt="Solar Garlic Logo" height={50} width={100} />
         </Link>
-        <NavLinks items={items} linkSize="text-4xl" />
-        <SocialLinks />
-      </nav>
-      {/* nav for mobile */}
-      <nav className={`xl:hidden flex flex-col ${teko.className}`}>
-        <div className="flex flex-row justify-between items-center h-24 px-8 bg-purple-dark border-b-2 border-b-purple-light">
-          <Link
-            href="/"
-            className="text-2xl text-yellow flex flex-row gap-2 h-full ml-2"
-          >
-            <Image src={logo} alt="Solar Garlic Logo" height={50} width={100} />
-          </Link>
-          {isMenuOpen ? (
-            <Image
-              onClick={closeMenu}
-              src={closeIcon}
-              height={40}
-              width={40}
-              alt="Exit icon"
-            />
-          ) : (
-            <Image
-              onClick={openMenu}
-              src={hamburgerIcon}
-              height={30}
-              width={30}
-              alt="Hamburger icon"
-            />
-          )}
-        </div>
-        {isMenuOpen && (
-          <div className="bg-purple-dark border-b-2 border-b-purple-light p-4">
-            {/* // menu drawer with nav links */}
-            <NavLinks
-              mobile
-              items={items}
-              linkSize="text-4xl"
-              onClick={closeMenu}
-            />
-          </div>
+        {isMenuOpen ? (
+          <Image
+            onClick={closeMenu}
+            src={closeIcon}
+            height={40}
+            width={40}
+            alt="Exit icon"
+          />
+        ) : (
+          <Image
+            onClick={openMenu}
+            src={hamburgerIcon}
+            height={30}
+            width={30}
+            alt="Hamburger icon"
+          />
         )}
-      </nav>
-    </>
+      </div>
+      {isMenuOpen && (
+        <div className="bg-purple-dark border-b-2 border-b-purple-light p-4">
+          {/* // menu drawer with nav links */}
+          <NavLinks
+            mobile
+            items={items}
+            linkSize="text-4xl"
+            onClick={closeMenu}
+          />
+        </div>
+      )}
+    </nav>
   );
 }
