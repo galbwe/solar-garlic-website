39bf99809f3364f9f06de7ec634f351029321357
Wes Galbraith <wes@Wess-MacBook-Pro.local>
2025-01-11 10:34:55 -0700

    Add commented section to tag next build for dockerhub

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,4 +5,8 @@ TIMESTAMP=$(date +%s)
 
 TAG=$TIMESTAMP-$LAST_COMMIT_SHA
 
+# for aws ecr
 docker build -t solar-garlic-next:$TAG .
+
+# for dockerhub
+# docker build -t galbwe92/solargarlicwebsite:$TAG .
