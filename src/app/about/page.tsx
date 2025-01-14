1d1313a4268771a038eca2382ec305358d0ad2af
Wes Galbraith <wes@macbookpro.lan>
2025-01-13 23:56:05 -0700

    Reduce about page x margin on mobile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,10 +4,10 @@ import { teko } from "@/fonts";
 
 export default function About() {
   return (
-    <div className="w-full flex flex-row flex-wrap justify-center items-center gap-1">
-      <section className="mt-8 p-4 md:p-12 text-2xl w-5/6 lg:w-4/5 2xl:w-2/5 flex flex-col gap-3 bg-purple-dark rounded border-2 border-purple-light">
+    <div className="w-screen flex flex-row flex-wrap justify-center items-center xl:gap-6 xl:mt-12">
+      <section className=" mt-4 xl:mt-8 p-4 md:p-12 text-2xl w-full lg:w-4/5 xl:w-3/5 2xl:w-2/5 flex flex-col gap-3 bg-purple-dark lg:rounded border-y-2 lg:border-2 border-purple-light">
         <h1
-          className={`${teko.className} text-5xl md:text-7xl text-yellow pb-2`}
+          className={`${teko.className} text-5xl md:text-7xl text-yellow pb-0 xl:pb-2`}
         >
           About
         </h1>
