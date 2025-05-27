180018cf6733ba50f9ac13463010910d2f7309e7
Wes Galbraith <galbwe92@gmail.com>
2025-05-26 22:12:07 -0600

    Create mailing list signup form and get it to call the create email endpoint

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,59 @@
+"use client";
+import { useState } from "react";
+
 import { CONTACT_INFO } from "@/constants";
 import { teko } from "@/fonts";
 
 export default function Contact() {
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState({ type: "", message: "" });
+
+  const handleEmailChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSubscribeToMailingList = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      // TODO: get the api host from environment variables
+      const res = await fetch("http://localhost:8080/api/v1/emails", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      if (res.ok) {
+        console.log("Success creating email ...");
+        setStatus({
+          type: "success",
+          message:
+            "Now one more step ... Please check your email and click the verification link to finish signing up.",
+        });
+        setEmail("");
+      } else {
+        console.error("Error creating email. Received error status code.");
+        setStatus({
+          type: "error",
+          message:
+            "Uh oh, there was a problem subscribing that email. Please try again.",
+        });
+      }
+    } catch (err) {
+      console.error("Error creating email");
+      console.error(err);
+      setStatus({
+        type: "error",
+        message:
+          "Uh oh, there was a problem subscribing that email. Please try again.",
+      });
+    }
+  };
+
   return (
     <section className="w-full h-full flex flex-col justify-start items-center pt-8">
       <div className="bg-purple-dark w-full max-w-5xl xl:w-2/3 flex flex-col justify-start items-center gap-6 py-4 border-2 border-purple-light rounded">
@@ -28,6 +80,37 @@ export default function Contact() {
             );
           })}
         </ul>
+      </div>
+      <div className="bg-purple-dark w-full max-w-5xl xl:w-2/3 flex flex-col justify-start items-center gap-6 py-4 border-2 border-purple-light rounded">
+        <h2
+          className={`${teko.className} text-yellow text-5xl self-center text-center`}
+        >
+          Join Our Mailing List
+        </h2>
+        <form
+          onSubmit={handleSubscribeToMailingList}
+          className="space-y-4 w-full"
+        >
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+            className="block w-full p-2 border text-black"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Sign Up
+          </button>
+          {
+            // TODO: add styling depending on status type
+            status?.message && <p>{status.message}</p>
+          }
+        </form>
       </div>
     </section>
   );
